4f972f37bfbb244754a0fba03276d4e107b13b2b
ncehk2019 <ncehk2019@gmail.com>
2019-06-12 16:52:20 +0800

    rthk live -> oncc live

--- precache-manifest.d8c82470aa065c6871be68b09114cb02.js
+++ precache-manifest.d8c82470aa065c6871be68b09114cb02.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "43a3b38ebaa33bac706d023062460e6b",
+    "revision": "fee813bafa795b93f4e2ea28566472ac",
     "url": "/nce-live/index.html"
   },
   {
-    "revision": "84d789f5f61734e4952a",
+    "revision": "45e7d6bd35be7507192d",
     "url": "/nce-live/static/css/main.f67476f8.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/nce-live/static/js/2.eb61e9e5.chunk.js"
   },
   {
-    "revision": "84d789f5f61734e4952a",
-    "url": "/nce-live/static/js/main.4f86ce91.chunk.js"
+    "revision": "45e7d6bd35be7507192d",
+    "url": "/nce-live/static/js/main.dd17a001.chunk.js"
   },
   {
     "revision": "f08b8f1706a257a4ebe8",
